be6887922dcd29fd5dbdbaf7375ce13ea18397e4
OPTIC7409 <79322893+OPTIC7409@users.noreply.github.com>
2023-11-04 23:13:27 +0000

    Create structs.go

---
+++ backend/structs/structs.go
@@ -0,0 +1,20 @@
+package structs
+
+type ErrorResponse struct {
+	ErrorCode    int    `json:"error_code"`
+	ErrorMessage string `json:"error_message"`
+}
+
+type HashedAndSaltedPassword struct {
+	HashedPassword string
+	RandomSalt     string
+}
+
+type SuccessResponse struct {
+	Success bool `json:"success"`
+}
+
+type UserResponse struct {
+	Username string `json:"username"`
+	Ranking  int    `json:"ranking"`
+}
