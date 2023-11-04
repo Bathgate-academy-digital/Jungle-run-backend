4a26a0a059893d229bc136b76be89635afcb6a79
OPTIC7409 <79322893+OPTIC7409@users.noreply.github.com>
2023-11-04 23:13:22 +0000

    Account Endpoints

---
+++ backend/endpoints/account.go
@@ -0,0 +1,36 @@
+package endpoints
+
+import (
+	"ba-digital/backend/endpoints/account"
+	//"ba-digital/backend/endpoints/account/update"
+	ReturnModule "ba-digital/backend/modules/return_module"
+	"net/http"
+)
+
+func ManageAccounts(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		//account.PostRequest(w, r)
+	} else if r.Method == "GET" {
+		account.GetRequest(w, r)
+	} else if r.Method == "DELETE" {
+		//account.DeleteRequest(w, r)
+	} else {
+		ReturnModule.MethodNotAllowed(w, r)
+	}
+}
+
+func ManageSessions(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		//session.GetRequest(w, r)
+	} else {
+		ReturnModule.MethodNotAllowed(w, r)
+	}
+}
+
+func ManageUpdate(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		//update.PostRequest(w, r)
+	} else {
+		ReturnModule.MethodNotAllowed(w, r)
+	}
+}
---
+++ backend/endpoints/account/get.go
@@ -0,0 +1,32 @@
+package account
+
+import (
+	"ba-digital/backend/database"
+	ReturnModule "ba-digital/backend/modules/return_module"
+	"net/http"
+)
+
+func GetRequest(w http.ResponseWriter, r *http.Request) {
+	sessionAuthentication := r.Header.Get("session")
+	username := r.Header.Get("username")
+
+	if sessionAuthentication != "" {
+		accountDataStruct := database.GetAccountDataFromSession(sessionAuthentication)
+
+		if accountDataStruct.Username == "" {
+			ReturnModule.InternalServerError(w, r)
+		} else {
+			ReturnModule.AccountData(w, r, accountDataStruct)
+		}
+	} else if username != "" {
+		accountDataStruct := database.GetAccountData(username)
+
+		if accountDataStruct.Username == "" {
+			ReturnModule.InternalServerError(w, r)
+		} else {
+			ReturnModule.AccountData(w, r, accountDataStruct)
+		}
+	} else {
+		ReturnModule.MissingData(w, r)
+	}
+}
